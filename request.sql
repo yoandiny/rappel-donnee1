820ecb1fdd50231cc6af3ea574050787760abe25
Yoan <yoandiny14@gmail.com>
2025-11-15 22:08:47 +0300

    completed question 1,2 and 3

---
+++ request.sql
@@ -0,0 +1,17 @@
+# Afficher l’id, first_name, last_name des employés qui n’ont pas d’équipe.
+
+SELECT id, first_name, last_name
+FROM Employee
+WHERE team_id IS NULL
+
+# Afficher l’id, first_name, last_name des employés qui n’ont jamais pris de congé de leur vie.
+select Employee.id, first_name, last_name from Employee
+left join Leave on Employee.id = leave.employee_id 
+where leave.employee_id is null
+
+# Afficher les congés de tel sorte qu’on voie l’id du congé, le début du congé, la fin du congé, le nom & prénom de l’employé qui prend congé et le nom de son équipe.
+
+select leave.id, leave.start_date, leave.end_date , Employee.last_name, Employee.first_name, Team.name as team_name
+from Leave
+join Employee on leave.employee_id = Employee.id
+join Team on Team.id = Employee.team_id
